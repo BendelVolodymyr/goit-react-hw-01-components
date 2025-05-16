32ce95e71be98895decde5d7fa0498d63c668d74
Volodymyr Bendel <134164183+BendelVolodymyr@users.noreply.github.com>
2025-05-16 04:35:06 +0200

    Delete dist directory

--- dist/FriendList/FriendListItem.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-const FriendListItem = ({ avatar, name, isOnline, }) => {
-    return ((0, jsx_runtime_1.jsxs)("li", { className: "item", children: [isOnline ? ((0, jsx_runtime_1.jsx)("span", { className: "status-on" })) : ((0, jsx_runtime_1.jsx)("span", { className: "status-off" })), (0, jsx_runtime_1.jsx)("img", { className: "avatar", src: avatar, alt: "User avatar", width: "48" }), (0, jsx_runtime_1.jsx)("p", { className: "name", children: name })] }));
-};
-exports.default = FriendListItem;
-//# sourceMappingURL=FriendListItem.js.map
--- dist/TransactionHistory/TransactionHistory.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-const TransactionHistory = ({ items }) => {
-    const resultItems = items.map(({ id, type, amount, currency }) => ((0, jsx_runtime_1.jsxs)("tr", { children: [(0, jsx_runtime_1.jsx)("td", { children: type.toUpperCase() }), (0, jsx_runtime_1.jsx)("td", { children: amount.toUpperCase() }), (0, jsx_runtime_1.jsx)("td", { children: currency.toUpperCase() })] }, id)));
-    return ((0, jsx_runtime_1.jsx)("section", { className: "transaction", children: (0, jsx_runtime_1.jsxs)("table", { className: "transaction-history", children: [(0, jsx_runtime_1.jsx)("thead", { children: (0, jsx_runtime_1.jsxs)("tr", { children: [(0, jsx_runtime_1.jsx)("th", { children: "Type" }), (0, jsx_runtime_1.jsx)("th", { children: "Amount" }), (0, jsx_runtime_1.jsx)("th", { children: "Currency" })] }) }), (0, jsx_runtime_1.jsx)("tbody", { children: resultItems })] }) }));
-};
-exports.default = TransactionHistory;
-//# sourceMappingURL=TransactionHistory.js.map
